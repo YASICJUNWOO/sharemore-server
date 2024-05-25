a8034140c8db0bb6bab3237664b205874b8cd22c
YASICJUNWOO <joonoo3@naver.com>
2024-05-26 00:19:53 +0900

    test : 회원 가입 test code

--- src/test/java/sharemore/sharemoreserver/domain/member/service/MemberServiceTest.java
+++ src/test/java/sharemore/sharemoreserver/domain/member/service/MemberServiceTest.java
@@ -1,17 +1,34 @@
 package sharemore.sharemoreserver.domain.member.service;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import sharemore.sharemoreserver.domain.member.Member;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
+
+@SpringBootTest
 class MemberServiceTest {
 
+    @Autowired
+    MemberService memberService;
+
     @Test
-    void test() {
-        // Given
+    void 회원가입() {
 
-        // When
+        //given
+        Member member = Member.builder()
+                .email("testEmail")
+                .password("testPassword").build();
 
-        // Then
+        //when
+        Member savedMember = memberService.join(member);
+
+        //then
+        assertThat(member).isEqualTo(savedMember);
+
     }
+
 }
