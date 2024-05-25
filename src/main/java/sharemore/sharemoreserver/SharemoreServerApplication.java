2400cadef91438de8bdc25be0f0055fb5c8bf477
YASICJUNWOO <joonoo3@naver.com>
2024-05-25 21:14:31 +0900

    commit message

---
+++ src/main/java/sharemore/sharemoreserver/SharemoreServerApplication.java
@@ -0,0 +1,13 @@
+package sharemore.sharemoreserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharemoreServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharemoreServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sharemore/sharemoreserver/SharemoreServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sharemore.sharemoreserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharemoreServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
