fb123e57e4f1b0f7c12f060b7866123b6010e2db
YASICJUNWOO <joonoo3@naver.com>
2024-05-26 00:30:39 +0900

    fix : 이메일로 회원 조회 로직 리팩토링

--- src/main/java/sharemore/sharemoreserver/domain/member/Member.java
+++ src/main/java/sharemore/sharemoreserver/domain/member/Member.java
@@ -5,12 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Entity
 @AllArgsConstructor
 @RequiredArgsConstructor
 @Builder
+@Getter
 public class Member {
 
     @Id
--- src/test/java/sharemore/sharemoreserver/domain/member/service/MemberServiceTest.java
+++ src/test/java/sharemore/sharemoreserver/domain/member/service/MemberServiceTest.java
@@ -1,20 +1,25 @@
 package sharemore.sharemoreserver.domain.member.service;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 import sharemore.sharemoreserver.domain.member.Member;
 
 import static org.assertj.core.api.Assertions.*;
 
 
 @SpringBootTest
+@Transactional
 class MemberServiceTest {
 
     @Autowired
     MemberService memberService;
 
+    @Rollback
     @Test
     void 회원가입() {
 
