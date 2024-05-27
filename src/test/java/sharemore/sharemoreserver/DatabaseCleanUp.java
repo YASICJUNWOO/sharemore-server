d18ee4ec14d75eadb8cf48ca3e29f920940bb222
YASICJUNWOO <joonoo3@naver.com>
2024-05-27 21:50:48 +0900

    test : 로그인 실패 test code

--- src/test/java/sharemore/sharemoreserver/DatabaseCleanUp.java
+++ src/test/java/sharemore/sharemoreserver/DatabaseCleanUp.java
@@ -56,7 +56,7 @@ public class DatabaseCleanUp implements InitializingBean {
 
         for (final String tableName : tableNames) {
             entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
-            entityManager.createNativeQuery("ALTER TABLE " + tableName + " ALTER COLUMN ID RESTART WITH 1").executeUpdate();
+            //entityManager.createNativeQuery("ALTER TABLE " + tableName + " ALTER COLUMN ID RESTART WITH 1").executeUpdate();
         }
 
         entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
--- src/test/java/sharemore/sharemoreserver/domain/member/service/MemberApiTest.java
+++ src/test/java/sharemore/sharemoreserver/domain/member/service/MemberApiTest.java
@@ -64,5 +64,38 @@ public class MemberApiTest extends ApiTest {
         assertThat(extract.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 
+    @Test
+    public void 로그인_실패_API() {
+
+        //given
+        Member member = Member.builder()
+                .email("testEmail")
+                .password("testPassword").build();
+
+        Member testMember = Member.builder()
+                .email("testEmail")
+                .password("testPassword2").build();
+
+        RestAssured.given().log().all()
+                .body(member)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/api/member/sign-up")
+                .then()
+                .log().all().extract();
+
+        //when
+        final ExtractableResponse<Response> extract = RestAssured.given().log().all()
+                .body(testMember)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/api/member/login")
+                .then()
+                .log().all().extract();
+
+        //then
+        assertThat(extract.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+    }
+
 
 }
