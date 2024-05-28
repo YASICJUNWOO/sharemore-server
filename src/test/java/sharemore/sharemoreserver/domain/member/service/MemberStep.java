a98f2fb38f76d670456053b67d029bcd045cab15
YASICJUNWOO <99794552+YASICJUNWOO@users.noreply.github.com>
2024-05-29 02:39:23 +0900

    refactor : RestAssured 기능 추출
    * feat : Member엔티티 생성
    
    * feat : MemberRepository 생성
    
    * feat : MemberService 생성
    
    * test : 회원 가입 test code
    
    * test : 회원 가입 로직 구현
    
    * test : 이메일로 회원 조회 test code 작성
    
    * feat : 이메일로 회원 조회 로직 구현
    
    * fix : 이메일로 회원 조회 로직 리팩토링
    
    * test : 회원 로그인 test code
    
    * feat : 회원 로그인 로직 구현
    
    * feat : api test를 위한 Rest Assured 도입
    
    * test : 회원가입 api test code
    
    * feat : 회원가입 api 로직 구현
    
    * test : test별 db 초기화를 위한 코드
    
    * test : 로그인 api test code
    
    * feat : 로그인 로직 구현
    
    * test : 로그인 실패 test code
    
    * test : 회원가입 중복 예외 test code
    
    * feat : 회원가입 중복 예외 구현
    
    * fix : 회원가입 중복 예외 test code hasMessageContaining로 수정
    
    * refactor : RestAssured 기능 추출

---
+++ src/test/java/sharemore/sharemoreserver/domain/member/service/MemberStep.java
@@ -0,0 +1,32 @@
+package sharemore.sharemoreserver.domain.member.service;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import sharemore.sharemoreserver.domain.member.Member;
+
+public class MemberStep {
+
+    public static ExtractableResponse<Response> 회원등록요청(final Member member){
+
+        return RestAssured.given().log().all()
+                .body(member)
+                .contentType("application/json")
+                .when()
+                .post("/api/member/sign-up")
+                .then()
+                .log().all().extract();
+    }
+
+    public static ExtractableResponse<Response> 로그인요청(final Member member){
+
+        return RestAssured.given().log().all()
+                .body(member)
+                .contentType("application/json")
+                .when()
+                .post("/api/member/login")
+                .then()
+                .log().all().extract();
+    }
+
+}
