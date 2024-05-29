edd6e3d788199f7e9d8df1fe7b10c10f7105a31e
YASICJUNWOO <joonoo3@naver.com>
2024-05-30 01:57:15 +0900

    refactor : 아이템 service 인터페이스화

--- src/main/java/sharemore/sharemoreserver/domain/item/ItemService.java
+++ src/main/java/sharemore/sharemoreserver/domain/item/ItemService.java
@@ -1,30 +1,12 @@
 package sharemore.sharemoreserver.domain.item;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 import sharemore.sharemoreserver.domain.item.dto.ItemRequest;
-import sharemore.sharemoreserver.domain.member.Member;
-import sharemore.sharemoreserver.domain.member.service.MemberService;
 
-@Service
-@RequiredArgsConstructor
-public class ItemService {
+public interface ItemService {
 
-    private final ItemRepository itemRepository;
-    private final MemberService memberService;
+    Item addItem(ItemRequest request);
 
-    public Item addItem(ItemRequest request) {
-        Member byEmail = memberService.findByEmail(request.getOwnerEmail());
-        Item item = request.toEntity(request, byEmail);
-        return addItem(item);
-    }
+    Item addItem(Item item);
 
-    public Item addItem(Item item) {
-        return itemRepository.save(item);
-    }
-
-    public Item findItemById(Long id) {
-        return itemRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 아이템이 존재하지 않습니다."));
-    }
+    Item findItemById(Long id);
 }
---
+++ src/main/java/sharemore/sharemoreserver/domain/item/ItemServiceImpl.java
@@ -0,0 +1,33 @@
+package sharemore.sharemoreserver.domain.item;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import sharemore.sharemoreserver.domain.item.dto.ItemRequest;
+import sharemore.sharemoreserver.domain.member.Member;
+import sharemore.sharemoreserver.domain.member.service.MemberService;
+
+@Service
+@RequiredArgsConstructor
+public class ItemServiceImpl implements ItemService{
+
+    private final ItemRepository itemRepository;
+    private final MemberService memberService;
+
+    @Override
+    public Item addItem(ItemRequest request) {
+        Member byEmail = memberService.findByEmail(request.getOwnerEmail());
+        Item item = request.toEntity(request, byEmail);
+        return addItem(item);
+    }
+
+    @Override
+    public Item addItem(Item item) {
+        return itemRepository.save(item);
+    }
+
+    @Override
+    public Item findItemById(Long id) {
+        return itemRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 아이템이 존재하지 않습니다."));
+    }
+}
