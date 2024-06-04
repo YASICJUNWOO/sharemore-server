c5fb55f396c5b186944b453be25f2fd4947b9d20
YASICJUNWOO <joonoo3@naver.com>
2024-06-04 23:04:03 +0900

    test : 중복 예약 방지 test code

--- src/test/java/sharemore/sharemoreserver/domain/reservation/ReservationServiceTest.java
+++ src/test/java/sharemore/sharemoreserver/domain/reservation/ReservationServiceTest.java
@@ -1,5 +1,6 @@
 package sharemore.sharemoreserver.domain.reservation;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +15,7 @@ import sharemore.sharemoreserver.domain.reseration.service.ReservationService;
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 @SpringBootTest
 @Transactional
@@ -100,6 +102,132 @@ public class ReservationServiceTest {
         // then
         assertThat(foundReservation).isEqualTo(savedReservation);
 
+    }
+
+    @Test
+    @DisplayName("중복 예약 방지 - 기존 예약의 시작 시간이 끝 시간일 때")
+    public void 중복_예약_방지1() {
+
+        // given
+        Member member = Member.builder()
+                .email("testEmail")
+                .password("testPassword")
+                .name("testName")
+                .phone("010-1234-5678")
+                .build();
+
+        Item item = Item.builder()
+                .title("아이템1")
+                .description("아이템1 설명")
+                .category("의류")
+                .price(10000)
+                .owner(member)
+                .build();
+
+        makeBasicReservation(member, item);
+
+        // when
+        Reservation duplicateReservation = Reservation.builder()
+                .member(member)
+                .item(item)
+                .startDateTime(LocalDateTime.parse("2024-02-15T09:00:00"))
+                .endDateTime(LocalDateTime.parse("2024-02-15T10:00:00"))
+                .build();
+
+        // then
+        assertThatThrownBy(() -> reservationService.addReservation(duplicateReservation))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("이미 예약된 시간입니다.");
+
+    }
+
+    @Test
+    @DisplayName("중복 예약 방지 - 기존 예약의 끝 시간이 시작 시간일 때")
+    public void 중복_예약_방지2() {
+
+        // given
+        Member member = Member.builder()
+                .email("testEmail")
+                .password("testPassword")
+                .name("testName")
+                .phone("010-1234-5678")
+                .build();
+
+        Item item = Item.builder()
+                .title("아이템1")
+                .description("아이템1 설명")
+                .category("의류")
+                .price(10000)
+                .owner(member)
+                .build();
+
+        makeBasicReservation(member, item);
+
+        // when
+        Reservation duplicateReservation = Reservation.builder()
+                .member(member)
+                .item(item)
+                .startDateTime(LocalDateTime.parse("2024-02-15T11:00:00"))
+                .endDateTime(LocalDateTime.parse("2024-02-15T12:00:00"))
+                .build();
+
+        // then
+        assertThatThrownBy(() -> reservationService.addReservation(duplicateReservation))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("이미 예약된 시간입니다.");
+
+    }
+
+    @Test
+    @DisplayName("중복 예약 방지 - 기존 예약에 겹치는 시간으로 예약할 때")
+    public void 중복_예약_방지3() {
+
+        // given
+        Member member = Member.builder()
+                .email("testEmail")
+                .password("testPassword")
+                .name("testName")
+                .phone("010-1234-5678")
+                .build();
+
+        Item item = Item.builder()
+                .title("아이템1")
+                .description("아이템1 설명")
+                .category("의류")
+                .price(10000)
+                .owner(member)
+                .build();
+
+        makeBasicReservation(member, item);
+
+        // when
+        Reservation duplicateReservation = Reservation.builder()
+                .member(member)
+                .item(item)
+                .startDateTime(LocalDateTime.parse("2024-02-15T09:00:00"))
+                .endDateTime(LocalDateTime.parse("2024-02-15T10:30:00"))
+                .build();
+
+        // then
+        assertThatThrownBy(() -> reservationService.addReservation(duplicateReservation))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("이미 예약된 시간입니다.");
+
+    }
+
+    private void makeBasicReservation(Member member, Item item) {
+
+        memberService.join(member);
+        itemService.addItem(item);
+
+        Reservation reservation = Reservation.builder()
+                .member(member)
+                .item(item)
+                .startDateTime(LocalDateTime.parse("2024-02-15T10:00:00"))
+                .endDateTime(LocalDateTime.parse("2024-02-15T11:00:00"))
+                .build();
+
+        reservationService.addReservation(reservation);
     }
 
 }
