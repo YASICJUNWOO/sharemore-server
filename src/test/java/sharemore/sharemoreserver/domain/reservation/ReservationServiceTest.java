f3b8176343634156ddb4f7d0c0364eb6d10332db
YASICJUNWOO <joonoo3@naver.com>
2024-06-04 18:54:01 +0900

    test : reservation id로 조회 test code

--- src/test/java/sharemore/sharemoreserver/domain/reservation/ReservationServiceTest.java
+++ src/test/java/sharemore/sharemoreserver/domain/reservation/ReservationServiceTest.java
@@ -63,4 +63,43 @@ public class ReservationServiceTest {
 
     }
 
+    @Test
+    public void 예약_ID로_조회() {
+
+        // given
+        Member member = Member.builder()
+                .email("testEmail")
+                .password("testPassword")
+                .name("testName")
+                .phone("010-1234-5678")
+                .build();
+
+        Item item = Item.builder()
+                .title("아이템1")
+                .description("아이템1 설명")
+                .category("의류")
+                .price(10000)
+                .owner(member)
+                .build();
+
+        memberService.join(member);
+        itemService.addItem(item);
+
+        Reservation reservation = Reservation.builder()
+                .member(new Member())
+                .item(new Item())
+                .startDateTime(LocalDateTime.now())
+                .endDateTime(LocalDateTime.now().plusDays(1))
+                .build();
+
+        Reservation savedReservation = reservationService.addReservation(reservation);
+
+        // when
+        Reservation foundReservation = reservationService.findById(savedReservation.getId());
+
+        // then
+        assertThat(foundReservation).isEqualTo(savedReservation);
+
+    }
+
 }
