ab1a30ce5994663cc406cf4df4dd1dfcaa24624c
YASICJUNWOO <99794552+YASICJUNWOO@users.noreply.github.com>
2024-06-05 21:30:33 +0900

    refactor : reservation 시간 형 변환

--- src/test/java/sharemore/sharemoreserver/domain/reservation/ReservationApiTest.java
+++ src/test/java/sharemore/sharemoreserver/domain/reservation/ReservationApiTest.java
@@ -14,6 +14,7 @@ import sharemore.sharemoreserver.domain.member.service.MemberStep;
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 @Transactional
 public class ReservationApiTest extends ApiTest {
@@ -45,8 +46,8 @@ public class ReservationApiTest extends ApiTest {
         ReservationRequest reservationRequest = ReservationRequest.builder()
                 .memberEmail("testEmail")
                 .itemId(1L)
-                .startDateTime(LocalDateTime.parse("2024-01-15T10:00:00"))
-                .endDateTime(LocalDateTime.parse("2024-01-15T10:30:00"))
+                .startDateTime("2024-01-15T10:00:00")
+                .endDateTime("2024-01-15T10:30:00")
                 .build();
 
         // when
@@ -56,5 +57,54 @@ public class ReservationApiTest extends ApiTest {
         assertThat(response.statusCode()).isEqualTo(201);
 
     }
+
+    //todo: 예약 중복 등록 테스트
+    /*@Test
+    @Rollback
+    public void 예약_중복_등록_API() {
+
+        // given
+        Member member = Member.builder()
+                .email("testEmail")
+                .password("testPassword")
+                .name("testName")
+                .phone("010-1234-5678")
+                .build();
+
+        MemberStep.회원등록요청(member);
+
+        ItemRequest item = ItemRequest.builder()
+                .title("아이템1")
+                .description("아이템1 설명")
+                .category("의류")
+                .price(10000)
+                .ownerEmail("testEmail")
+                .build();
+
+        ItemStep.아이템등록요청(item);
+
+        ReservationRequest reservationRequest = ReservationRequest.builder()
+                .memberEmail("testEmail")
+                .itemId(1L)
+                .startDateTime(LocalDateTime.parse("2024-01-15T10:00:00"))
+                .endDateTime(LocalDateTime.parse("2024-01-15T10:30:00"))
+                .build();
+
+        ReservationStep.예약생성요청(reservationRequest);
+
+        // when
+        ReservationRequest duplicateReservationRequest = ReservationRequest.builder()
+                .memberEmail("testEmail")
+                .itemId(1L)
+                .startDateTime(LocalDateTime.parse("2024-01-15T09:30:00"))
+                .endDateTime(LocalDateTime.parse("2024-01-15T10:10:00"))
+                .build();
+
+        // then
+        assertThatThrownBy(() -> ReservationStep.예약생성요청(duplicateReservationRequest))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("이미 예약된 시간입니다.");
+
+    }*/
 
 }
